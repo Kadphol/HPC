379af3a04617c14171f672db7389e7f4260881a7
Kadphol <kadphol.len@mail.kmutt.ac.th>
2020-05-31 21:54:23 +0700

    Add lab7.1.cu

---
+++ lab7/lab7.1.cu
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cuda.h>
+
+int main()
+{
+    int nDevices;
+
+    cudaGetDeviceCount(&nDevices);
+    for(int i = 0; i < nDevices; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+        printf("  Memory Clock Rate (KHz): %d\n",
+            prop.memoryClockRate);
+        printf("  Memory Bus Width (bits): %d\n",
+            prop.memoryBusWidth);
+        printf("  Peak Memory Bandwidth (GB/s): %f\n",
+            2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+        printf("  Multiprocessor count: %d\n",
+            prop.multiProcessorCount);
+        printf("  Maximum Thread per Block: %d\n\n",
+            prop.maxThreadsPerBlock);
+
+    }
+}
